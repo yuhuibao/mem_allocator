d8f494d7b5e3bb964e14ebf4788de6eded3563bc
Yuhui <julie.bao32@gmail.com>
2020-03-15 17:35:19 -0400

    done free

--- opt_malloc.c
+++ opt_malloc.c
@@ -6,7 +6,8 @@
 #define BLOCK_MEM(ptr) ((void*)((unsigned long long)ptr+sizeof(header)))
 #define BLOCK_HEADER(ptr) ((node*)((unsigned long long)ptr-sizeof(header)))
 typedef struct node_t{
-    size_t size; // size of the free mem for this block
+    int free;
+    size_t size; // size of allocated mem for users but actually allocated size is size+header+footer
     struct node_t* next;
     struct node_t* prev;
 }node;
@@ -61,6 +62,10 @@ and return this new block */
 node* split(node* ptr_b, size_t size){
     node* newptr_b = (node*)((unsigned long long)BLOCK_MEM(ptr_b) + size + sizeof(footer));
     newptr_b->size =  ptr_b->size - size - sizeof(header) - sizeof(footer);
+    newptr_b->free = 0;
+    //add footer to the new free block
+    footer* new_f = (footer*)((unsigned long long)newptr_b + sizeof(header) + newptr_b->size);
+    new_f->size = newptr_b->size;
     // add header and footer to the allocated mem block
     header* block_allo = (header*)ptr_b;
     block_allo->size = size;
@@ -76,9 +81,9 @@ xmalloc(size_t bytes)
     void* block_mem;
     node *curr=head, *newptr;
     while(curr){
-        if(curr->size >= bytes + sizeof(header) + sizeof(footer)){
+        if(curr->size >= bytes){
             block_mem = BLOCK_MEM(curr);
-            if(curr->size > bytes + sizeof(header) + sizeof(footer)){
+            if(curr->size > bytes){
                 newptr = split(curr,bytes);
                 // the new block just stick to the original place, no need to keep sorted
                 fl_replace(newptr,curr);
@@ -95,7 +100,7 @@ xmalloc(size_t bytes)
     assert_ok((long)curr,"mmap");
     curr->next = NULL;
     curr->prev = NULL;
-    curr->size = 4096;
+    curr->size = 4096 - sizeof(header) - sizeof(footer);
     newptr = split(curr, bytes);
     fl_replace(newptr, curr);
 
@@ -109,6 +114,8 @@ xfree(void* ptr)
     /*locate the prev and next free blocks*/
     // header of the current block
     header* h = (header*) (p - sizeof(header));
+    node* curr = (node*)h;
+    curr->free = 0;
     // header of the next block
     header* hn = (header*) (p + h->size + sizeof(footer));
     // previous footer
@@ -116,7 +123,42 @@ xfree(void* ptr)
     // previous header
     header* hp = (header*)((unsigned long long)f - f->size - sizeof(header));
 
+    node *next, *prev;
 
+	if (hn->free == 0) {  // combine with the next free block
+		next = (node *) hn;
+		curr->next = next->next;       
+        curr->prev = next->prev;
+
+		next->next->prev = curr;
+        next->prev->next = curr;
+
+		curr->size += next->size + sizeof(header) + sizeof(footer);
+
+		((footer*) ((unsigned long long)curr + sizeof(header) + curr->size))->size = curr->size;
+	}
+
+	if (hp->free == 0) { // combine with the previous free block
+		prev = (node *) hp;
+		prev->size += curr->size + sizeof(header) + sizeof(footer);
+		((footer *) ((unsigned long long)prev + sizeof(header) + prev->size))->size = prev->size;
+	}
+
+	if (hp->free!=0 && hn->free!=0) {
+		// add the new free block to the head of the free list
+        if(!head){
+            head = curr;
+            curr->prev = NULL;
+            curr->next = NULL;
+        }else{
+            if(head->next){
+                head->next->prev = curr;
+            }   
+            curr->next = head->next;
+            head = curr;
+            curr->prev = NULL;
+        }
+    }
 }
 
 void*
